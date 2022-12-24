ffb49bc8f0a4a2270007b9ccdf5ce44d2d7c20f9
DipShtick <vancouver_cliparf@simplelogin.com>
2022-12-24 12:28:28 +0100

    fix 2.0

--- Assets/Scripts/HPbonus.cs
+++ Assets/Scripts/HPbonus.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HPbonus : MonoBehaviour
+public class hpBonus : MonoBehaviour
 {
     float bonus;
     void Awake()
